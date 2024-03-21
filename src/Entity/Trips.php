6410aae0de932648b49f697bf3835ae1d253fbe9
SamLion1 <morpheus9@free.fr>
2024-03-21 14:25:08 +0100

    MàJ

---
+++ src/Entity/Trips.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TripsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TripsRepository::class)]
+class Trips
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $tripDate = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $origin = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $destination = null;
+
+    #[ORM\Column]
+    private ?int $mileage = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $unit = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $context = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $category = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?bool $billableClient = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTripDate(): ?\DateTimeInterface
+    {
+        return $this->tripDate;
+    }
+
+    public function setTripDate(\DateTimeInterface $tripDate): static
+    {
+        $this->tripDate = $tripDate;
+
+        return $this;
+    }
+
+    public function getOrigin(): ?string
+    {
+        return $this->origin;
+    }
+
+    public function setOrigin(string $origin): static
+    {
+        $this->origin = $origin;
+
+        return $this;
+    }
+
+    public function getDestination(): ?string
+    {
+        return $this->destination;
+    }
+
+    public function setDestination(string $destination): static
+    {
+        $this->destination = $destination;
+
+        return $this;
+    }
+
+    public function getMileage(): ?int
+    {
+        return $this->mileage;
+    }
+
+    public function setMileage(int $mileage): static
+    {
+        $this->mileage = $mileage;
+
+        return $this;
+    }
+
+    public function getUnit(): ?string
+    {
+        return $this->unit;
+    }
+
+    public function setUnit(string $unit): static
+    {
+        $this->unit = $unit;
+
+        return $this;
+    }
+
+    public function getContext(): ?string
+    {
+        return $this->context;
+    }
+
+    public function setContext(string $context): static
+    {
+        $this->context = $context;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isBillableClient(): ?bool
+    {
+        return $this->billableClient;
+    }
+
+    public function setBillableClient(bool $billableClient): static
+    {
+        $this->billableClient = $billableClient;
+
+        return $this;
+    }
+}
